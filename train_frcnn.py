10cd53525ccf3390023a9581e7b3bf448f11949c
tobiolabode <tobi@tobiolabode.com>
2020-07-04 20:00:42 +0100

    fixing OMP probelm

--- train_frcnn.py
+++ train_frcnn.py
@@ -16,6 +16,10 @@ from keras_frcnn import losses as losses
 import keras_frcnn.roi_helpers as roi_helpers
 from keras.utils import generic_utils
 
+import os
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
 sys.setrecursionlimit(40000)
 
 parser = OptionParser()
