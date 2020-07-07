13fae1979ef492e67aa3afacce087404847d5a5d
tobiolabode <tobi@tobiolabode.com>
2020-07-07 20:47:01 +0100

    writed results images to folder

--- test_frcnn.py
+++ test_frcnn.py
@@ -255,4 +255,4 @@ for idx, img_name in enumerate(sorted(os.listdir(img_path))):
     print(all_dets)
     cv2.imshow('img', img)
     cv2.waitKey(0)
-    # cv2.imwrite('./results_imgs/{}.png'.format(idx),img)
+    cv2.imwrite('./results_imgs/{}.png'.format(idx), img)
